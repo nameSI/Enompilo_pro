2964a487c869bfb1638aa58badf1f2c745c00d66
s218156871 <s218156871@R126-01>
2023-05-29 10:29:27 +0200

    Add project files.

---
+++ Enompilo_pro/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enompilo_pro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Enompilo_pro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enompilo_pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
